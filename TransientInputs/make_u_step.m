ec28a16a3a9650fce3f63c5107c4b04eb4965566
Lim Jing Xuan <jingx.lim@outlook.com>
2017-11-05 21:24:43 -0500

    fixed Seony's code: (1) transient inputs, (2) different compartments, (3) show dendritic filtering

--- TransientInputs/make_u_step.m
+++ TransientInputs/make_u_step.m
@@ -10,10 +10,12 @@
 % This function returns the input current at a given time t with a time
 % shift t_shift at the compartment c. It is used in dvdt.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [u_step] = make_u_step(t,t_shift,c,n)
+function [u_step] = make_u_step(t,t_shift,step_dur,c,n)
 u_step = zeros(n,1);
 
 if t <= t_shift 
+    u = 0;
+elseif t >= t_shift+step_dur;
     u = 0;
 else
     u = 1;
--- TransientInputs/transient_inputs.m
+++ TransientInputs/transient_inputs.m
@@ -15,47 +15,63 @@ dend(:,3:6) = dend(:,3:6)./1e4;
 
 % plot_tree(dend);
 
+%% Jing?
+jing = 1
+if jing    options = odeset('OutputFcn',@odewbar);    end
+
 %% variables
 % comp model
 t = 2; x = 3; y = 4; z = 5; r = 6; p = 7;
-Ri = 100; Rm = 10000; Cm = 1; Iapp = 1e-9;
+Ri = 100; Rm = 10000; Cm = 1; Iapp = 1e-7;
 n = size(dend,1);
 l = comp_len(dend);
 
 % transient inputs
 t_n = 100;
-t_span = linspace(1,1e4,t_n);
-t_shift = 25;
+t_span = linspace(1,5e4,t_n);
+t_shift = 0;
+step_dur = 100;
 c = 25;
 
-%% step input
-[A,B] = make_compartmental_matrices_simple(Ri,Rm,Cm,dend(:,r),l,n,dend(:,p));
+injects = round(linspace(142,173,10));
+max_amp = zeros(size(injects));
+for i=1:numel(injects)
+    c = injects(i);
+    % step input
+    [A,B] = make_compartmental_matrices_simple(Ri,Rm,Cm,dend(:,r),l,n,dend(:,p));
 
-% numerical (ode)
-dvdt_step = @(t,v) A*v + B*make_u_step(t,t_shift,c,n)*Iapp;
-[tp,v_step] = ode23(dvdt_step, t_span, zeros(n,1));
-v_step = v_step';
+    % numerical (ode)
+    dvdt_step = @(t,v) A*v + B*make_u_step(t,t_shift,step_dur,c,n)*Iapp;
+    [tp,v_step] = ode23(@(tp,v_step) dvdt_step(tp,v_step), t_span, zeros(n,1), options);
+    v_step = v_step';
 
-% ss
-u_step = zeros(n,1);
-u_step(c) = Iapp;
-v_step_ss = -inv(A)*B*u_step;
+    % ss
+    u_step = zeros(n,1);
+    u_step(c) = Iapp;
+    v_step_ss = -inv(A)*B*u_step;
 
-%% plot for various compartments (step)
-% change variables to plot
-T = tp./(Rm*Cm);
+    % plot for various compartments (step)
+    % change variables to plot
+    T = tp./(Rm*Cm);
 
-% numerical (ode)
-figure('Name','Models of the Neuron Project: Topic 3')
-hold on
-plot(T,v_step(1,:),'m')
-plot(T,v_step(c,:),'g')
-plot(T,v_step(c-2,:),'k')
-plot(T,v_step(c+2,:),'b')
-title('Voltage Given Unit Step Input')
-legend('Soma','Compartment w/Iapp',...
-    'Grandparent compartment','Grandchild compartment')
-xlabel('Time'); ylabel('Voltage (mV)');
+    % numerical (ode)
+    figure('Name','Models of the Neuron Project: Topic 3')
+    hold on
+    plot(T,v_step(1,:),'m', 'DisplayName', 'Soma')
+    plot(T,v_step(c,:),'g', 'DisplayName', 'cmprt '+string(c))
+    % plot(T,v_step(c-2,:),'k')
+    % plot(T,v_step(c+2,:),'b')
+    title('Voltage Given Unit Step Input')
+    legend('show')
+    xlabel('Time'); ylabel('Voltage (mV)');
+    
+    max_amp(i)= max(v_step(1,:))
+    saveas(gcf, strcat('output_step_',num2str(c),'.png'))
+end
+
+figure(); plot(injects, max_amp, 'o-');
+xlabel('Time'); ylabel('Peak voltage (mV)');
+saveas(gcf, strcat('amp_step_',num2str(c),'.png'))
 
 % ss
 % Note from Simon: I suggest plotting steady state the way I did it for the
@@ -84,48 +100,61 @@ xlabel('Time'); ylabel('Voltage (mV)');
 % end
 
 %% time varying synaptic conductance change
-[A,B,L,C] = make_compartmental_matrices(Ri,Rm,Cm,dend(:,r),l,n,dend(:,p));
+injects = round(linspace(142,173,10));
+max_amp_s = zeros(size(injects));
+for i=1:numel(injects)
+    c = injects(i);
+    [A,B,L,C] = make_compartmental_matrices(Ri,Rm,Cm,dend(:,r),l,n,dend(:,p));
 
-% numerical (ode)
-dvdt_exp = @(t,v) A*v + B*make_u_exp(t,t_shift,c,n)...
-    + make_G(make_u_exp(t,t_shift,c,n),C)*v;
-[tp,v_exp_s] = ode23(dvdt_exp, t_span, zeros(n,1));
-v_exp_s = v_exp_s';
-% adjust v_exp (it's actually v*)
-Er = ones(n,1)*(-70);           % mV
-Ee = ones(n,1)*(60);            % mV
-Ei = Er;                        % mV
-v_exp = zeros(size(v_exp_s));
-for i = 1:n
-    v_exp(i,:) = v_exp_s(i,:)*(Ee(i)-Ei(i))+Er(i);
+
+    % numerical (ode)
+    dvdt_exp = @(t,v) A*v + B*make_u_exp(t,t_shift,c,n)...
+        + make_G(make_u_exp(t,t_shift,c,n),C)*v;
+    [tp,v_exp_s] = ode23(@(tp, v_exp_s) dvdt_exp(tp, v_exp_s), t_span, zeros(n,1), options);
+    v_exp_s = v_exp_s';
+    % adjust v_exp (it's actually v*)
+    Er = ones(n,1)*(-70);           % mV
+    Ee = ones(n,1)*(60);            % mV
+    Ei = Er;                        % mV
+    v_exp = zeros(size(v_exp_s));
+    for i = 1:n
+        v_exp(i,:) = v_exp_s(i,:)*(Ee(i)-Ei(i))+Er(i);
+    end
+
+    % ss
+    % jx: no steady state for transient inputs
+    % % intialize
+    % u_exp = zeros(2*n,1);
+    % g = zeros(n,1);
+    % % put in values
+    % g(c) = 0.001;
+    % u_exp(1:2:end) = g;
+    % G = diag(-g./C);
+    % % ss voltage calculation
+    % v_exp_ss = -inv(A+G)*B*u_exp;
+
+    % plot for various compartments (exp)
+    % change variables
+    T = tp./(Rm*Cm);
+
+    % numerical (ode)
+    figure('Name','Models of the Neuron Project: Topic 3')
+    hold on
+    plot(T,v_exp(1,:),'m', 'DisplayName', 'Soma')
+    plot(T,v_exp(c,:),'g', 'DisplayName', 'cmprt '+string(c))
+    % plot(t_span,v_exp(c-2,:),'k')
+    % plot(t_span,v_exp(c+2,:),'b')
+    title('Voltage Given Decaying Exponential w/Time Varying Conductance Change')
+    legend('show')
+    xlabel('Time (us)'); ylabel('Voltage (mV)');
+    
+    max_amp_s(i)= max(v_step(1,:))
+    saveas(gcf, strcat('output_exp_',num2str(c),'.png'))
 end
 
-% ss
-% intialize
-u_exp = zeros(2*n,1);
-g = zeros(n,1);
-% put in values
-g(c) = 0.001;
-u_exp(1:2:end) = g;
-G = diag(-g./C);
-% ss voltage calculation
-v_exp_ss = -inv(A+G)*B*u_exp;
-
-%% plot for various compartments (exp)
-% change variables
-T = tp./(Rm*Cm);
-
-% numerical (ode)
-figure('Name','Models of the Neuron Project: Topic 3')
-hold on
-plot(t_span,v_exp(1,:),'m')
-plot(t_span,v_exp(c,:),'g')
-plot(t_span,v_exp(c-2,:),'k')
-plot(t_span,v_exp(c+2,:),'b')
-title('Voltage Given Decaying Exponential w/Time Varying Conductance Change')
-legend('Soma','Compartment w/Iapp',...
-    'Grandparent compartment','Grandchild compartment')
-xlabel('Time (us)'); ylabel('Voltage (mV)');
+figure(); plot(injects, max_amp, 'o-');
+xlabel('Time'); ylabel('Peak voltage (mV)');
+saveas(gcf, strcat('amp_exp_',num2str(c),'.png'))
 
 % ss
 % figure('Name','Models of the Neuron Project: Topic 3')
